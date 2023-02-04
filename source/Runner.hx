2c67fdaa110c65e73911ca302fd2a716a222b26f
Jaakko Rinta-Filppula <jaakko@r-f.fi>
2023-02-04 15:17:50 +0200

    Add runner animation

--- source/Runner.hx
+++ source/Runner.hx
@@ -1,12 +1,13 @@
 package;
 
 import flixel.FlxSprite;
-import flixel.util.FlxColor;
 
 class Runner extends FlxSprite {
 	public function new(x:Float, y:Float) {
 		super(x, y);
 
-		makeGraphic(70, 150, FlxColor.GRAY);
+		loadGraphic(AssetPaths.runner__png, true, 220, 220);
+		animation.add('run', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10);
+		animation.play('run');
 	}
 }
