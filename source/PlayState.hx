54c55f6529cd25c2c332b12db5dac70d9ce0e63e
Mika Kuitunen <mika.kuitunen@etasku.fi>
2023-02-04 18:31:04 +0200

    Better runner sprite and parallaxing bg

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,8 +8,13 @@ import flixel.addons.display.FlxBackdrop;
 class PlayState extends FlxState {
 	static inline var INITIAL_SPEED = 200.0;
 	static inline var DRAG = 5.0;
+	static inline var GRASS_BACK_SPEED_MULTIPLIER = 0.5;
+	static inline var RAFTERS_SPEED_MULTIPLIER = 0.3;
 
 	var background:FlxSprite;
+	var grassBack:FlxBackdrop;
+	var grassFront:FlxSprite;
+	var rafters:FlxBackdrop;
 	var track:FlxBackdrop;
 	var runner:Runner;
 	var player:Cheerleader;
@@ -17,16 +22,26 @@ class PlayState extends FlxState {
 	var speed:Float = INITIAL_SPEED;
 
 	override public function create() {
-		background = new FlxSprite(0, 0, AssetPaths.BG__png);
-		background.screenCenter();
-		background.scrollFactor.set(0, 0);
+		background = new FlxSprite(0, 0, AssetPaths.sky__png);
 		add(background);
 
+		grassBack = new FlxBackdrop(AssetPaths.grass_back__png, X);
+		grassBack.y = 188;
+		grassBack.velocity.x = -speed * GRASS_BACK_SPEED_MULTIPLIER;
+		add(grassBack);
+
+		grassFront = new FlxSprite(0, 377, AssetPaths.grass_front__png);
+		add(grassFront);
+
+		rafters = new FlxBackdrop(AssetPaths.rafters__png, X);
+		rafters.y = 85;
+		rafters.velocity.x = -speed * RAFTERS_SPEED_MULTIPLIER;
+		add(rafters);
+
 		track = new FlxBackdrop(AssetPaths.Track__png, X);
-		track.y = 240;
+		track.y = 250;
 		track.velocity.x = -speed;
 		track.drag.x = DRAG;
-		track.scrollFactor.set(1, 0);
 		add(track);
 
 		runner = new Runner(100, 100);
@@ -42,6 +57,8 @@ class PlayState extends FlxState {
 
 	override public function update(elapsed:Float) {
 		runner.setAnimSpeed(Math.abs(track.velocity.x));
+		grassBack.velocity.x = track.velocity.x * GRASS_BACK_SPEED_MULTIPLIER;
+		rafters.velocity.x = track.velocity.x * RAFTERS_SPEED_MULTIPLIER;
 		super.update(elapsed);
 	}
 }
